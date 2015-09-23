3123c429b17544d6e473989c8dd9f859a2cfa07b
Joaquin Marcher <joaquin@marcher.com.uy>
2015-09-23 17:35:21 -0300

    Se agrega la ventana de detalles del proyecto, incluyendo una clase para ordenar las columnas.

---
+++ Gestion de proyectos/GUI/Utils/DatosDePrueba.cs
@@ -0,0 +1,113 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI.Utils
+{
+    public class DatosDePrueba
+    {
+        private List<Proyecto> Proyectos { get; set; }
+        public DatosDePrueba()
+        {
+            Proyectos = new List<Proyecto>();
+        }
+
+        public List<Proyecto> ObtenerUnaListaProyectos()
+        {
+            Random r = new Random();
+            for (int i = 0; i < r.Next(30, 35); i++)
+            {
+                Proyectos.Add(CrearProyectoAleatorio());
+            }
+            return Proyectos;
+        }
+
+        private Tarea CrearTareaAleatoria(DateTime fecha, bool agregarSubTareas = false)
+        {
+            Random r = new Random();
+            Tarea t = new Tarea()
+            {
+                Nombre = "Tarea Nro: "+r.Next(1,3000),
+                FInicio =fecha.CompareTo(DateTime.Now)==0 ? fecha : fecha.AddDays(r.Next(1,30)),
+                Objetivo = "Objetivo",
+                Prioridad = r.Next(0,2),
+                
+            };
+            if(agregarSubTareas){
+                GenerarSubtareas(r, t);
+            }
+            return t;
+        }
+
+        private void GenerarSubtareas(Random r, Tarea t)
+        {
+            for (int i = 0; i < r.Next(0, 5); i++)
+            {
+                bool agregarMas = false;
+                if (r.Next(1, 10) > 8) agregarMas = true;
+                t.AgregarSubtarea(CrearTareaAleatoria(t.FInicio, agregarMas));
+            }
+        }
+
+        private Etapa CrearEtapaAleatoria()
+        {
+            Random r = new Random();
+            Etapa e = new Etapa()
+            {
+                Nombre = "Etapa Nro: "+r.Next(1,3000),
+                Id = GenerarIdUnico(),
+                
+            };
+
+            AgregarTareasAEtapa(r, e);
+            return e;
+        }
+
+        private void AgregarTareasAEtapa(Random r, Etapa e)
+        {
+            for (int i = 0; i < r.Next(1, 25); i++)
+            {
+                e.AgregarTarea(CrearTareaAleatoria(DateTime.Now,true));
+            }
+        }
+
+        private int GenerarIdUnico()
+        {
+            long i = 1;
+
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1);
+            }
+
+            int Numero = (int)((DateTime.Now.Ticks / 10) % 1000000000);
+
+            return Numero;
+        }
+
+        private Proyecto CrearProyectoAleatorio()
+        {
+            Random r = new Random();
+            Proyecto p = new Proyecto()
+            {
+                Nombre = "Proyecto Nro: "+r.Next(1,3000),
+                Objetivo = "Objetivo Nro: "+r.Next(1,35),
+                Id = GenerarIdUnico()
+            };
+
+            AgregarEtapasAProyecto(r, p);
+            return p;
+        }
+
+        private void AgregarEtapasAProyecto(Random r, Proyecto p)
+        {
+            for (int i = 0; i < r.Next(4, 30); i++)
+            {
+                p.AgregarEtapa(CrearEtapaAleatoria());
+            }
+        }
+    }
+}
---
+++ Gestion de proyectos/GUI/VentanaDetallesProyecto.cs
@@ -0,0 +1,100 @@
+﻿using Dominio;
+using GUI.Utils;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI
+{
+    public partial class VentanaDetallesProyecto : Form
+    {
+        private Proyecto proyecto;
+        private ListViewColumnSorter lvwColumnSorter;
+
+        public VentanaDetallesProyecto(Proyecto proyecto)
+        {
+            this.proyecto = proyecto;
+            InitializeComponent();
+            InicializarControles();
+        }
+
+        private void InicializarControles()
+        {
+            InicializarListViewSorter();
+            AsignarTitulo();
+            InicializarLista();
+            ActualizarLista();
+        }
+
+        private void InicializarListViewSorter()
+        {
+            lvwColumnSorter = new ListViewColumnSorter();
+            etapasListView.ListViewItemSorter = lvwColumnSorter;
+        }
+
+        private void AsignarTitulo()
+        {
+            this.Text = "Etapas de: " + proyecto.Nombre;
+        }
+
+        private void ActualizarLista()
+        {
+            etapasListView.Items.Clear();
+            foreach (Etapa e in proyecto.Etapas)
+            {
+                ListViewItem lvi = CrearNuevoItemListView(e);
+                etapasListView.Items.Add(lvi);
+                
+            }
+        }
+
+        private static ListViewItem CrearNuevoItemListView(Etapa e)
+        {
+            ListViewItem lvi = new ListViewItem();
+            lvi.Text = e.Nombre;
+            lvi.SubItems.Add(e.Tareas.Count.ToString());
+            lvi.SubItems.Add(e.Finalizada ? e.FechaFinalizacion.ToString() : "");
+            return lvi;
+        }
+
+        private void InicializarLista()
+        {
+            //propiedades de la listview
+            etapasListView.View = View.Details;
+            etapasListView.FullRowSelect = true;
+            etapasListView.GridLines = true;
+            etapasListView.Sorting = SortOrder.Ascending;
+            etapasListView.Columns.Add("Nombre", 100, HorizontalAlignment.Left);
+            etapasListView.Columns.Add("Cant. Tareas", 50, HorizontalAlignment.Left);
+            etapasListView.Columns.Add("Fecha finalización", 140, HorizontalAlignment.Left);
+        }
+
+        private void etapasListView_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == lvwColumnSorter.SortColumn)
+            {
+                if (lvwColumnSorter.Order == SortOrder.Ascending)
+                {
+                    lvwColumnSorter.Order = SortOrder.Descending;
+                }
+                else
+                {
+                    lvwColumnSorter.Order = SortOrder.Ascending;
+                }
+            }
+            else
+            {
+                lvwColumnSorter.SortColumn = e.Column;
+                lvwColumnSorter.Order = SortOrder.Ascending;
+            }
+
+            etapasListView.Sort();
+        }
+    }
+}
