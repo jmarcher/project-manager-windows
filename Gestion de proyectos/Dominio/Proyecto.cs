9ac06dc69f5e26a32728d034fb5c7a733f9eb130
joaquin <joaand4@gmail.com>
2015-09-23 02:31:28 -0300

    Se crea la clase Proyecto

---
+++ Gestion de proyectos/Dominio/Proyecto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Proyecto
+    {
+        public static string Espacio = " ";
+        public int Id { get; set; }
+
+        public String Nombre { get; set; }
+        public String Objetivo { get; set; }
+        public DateTime fechaFinalizado { get; set; }
+        public bool Finalizado { get; private set; }
+        public List<Etapa> Etapas{get; set;}
+
+        public Proyecto()
+        {
+            Etapas = new List<Etapa>();
+        }
+
+       
+    
+    }
+}
