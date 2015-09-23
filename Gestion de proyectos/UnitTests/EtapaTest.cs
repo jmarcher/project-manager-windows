5d561e09eb199a777a7e8456073896af5e00f1e2
Joaquin Marcher <joaquin@marcher.com.uy>
2015-09-22 23:34:06 -0300

    Agregada la clase de pruebas de etapa

---
+++ Gestion de proyectos/UnitTests/EtapaTest.cs
@@ -0,0 +1,40 @@
+﻿using Xunit;
+using Dominio;
+
+namespace UnitTests
+{
+    public class EtapaTest
+    {
+        [Fact]
+        public void MarcarEtapaComoFinalizada()
+        {
+            Tarea tarea = new Tarea() { Nombre = "Tarea" };
+            tarea.MarcarFinalizada();
+            Etapa etapa = new Etapa();
+            etapa.AgregarTarea(tarea);
+            etapa.MarcarFinalizada();
+            Assert.True(etapa.Finalizada);
+        }
+        [Fact]
+        public void MarcarEtapaComoFinalizadaConTareaSinFinalizar()
+        {
+            Tarea tarea = new Tarea() { Nombre = "Tarea" };
+            Etapa etapa = new Etapa();
+            etapa.AgregarTarea(tarea);
+            etapa.MarcarFinalizada();
+            Assert.False(etapa.Finalizada);
+        }
+        [Fact]
+        public void MarcarEtapaComoFinalizadaConTareaConYSinFinalizar()
+        {
+            Tarea tareaNoFinalizada = new Tarea() { Nombre = "Tarea sin terminar" };
+            Tarea tareaFinalizada = new Tarea() { Nombre = "Tarea terminada" };
+            tareaFinalizada.MarcarFinalizada();
+            Etapa etapa = new Etapa();
+            etapa.AgregarTarea(tareaNoFinalizada);
+            etapa.AgregarTarea(tareaFinalizada);
+            etapa.MarcarFinalizada();
+            Assert.False(etapa.Finalizada);
+        }
+    }
+}
