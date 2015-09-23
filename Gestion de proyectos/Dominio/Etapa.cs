a072fd3833a55e31be5f10260e7e63ac6ab601a2
Joaquin Marcher <joaquin@marcher.com.uy>
2015-09-22 23:52:38 -0300

    Agregado código para cumplir una de las pruebas de duración

--- Gestion de proyectos/Dominio/Etapa.cs
+++ Gestion de proyectos/Dominio/Etapa.cs
@@ -1,4 +1,4 @@
-ï»¿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,11 +8,32 @@ namespace Dominio
     public class Etapa
     {
         public int Id { get; set; }
+        public List<Tarea> Tareas { get; set; }
+
+        public Etapa()
+        {
+            Tareas = new List<Tarea>();
+        }
 
         public override bool Equals(object obj)
         {
             Etapa e = (Etapa)obj;
             return e.Id == this.Id;
+        }
+
+        public int CalcularDuracion()
+        {
+            int SumaDuracion = 0;
+            foreach (Tarea t in Tareas)
+            {
+                SumaDuracion += t.CalcularDuracion();
+            }
+            return SumaDuracion;
+        }
+
+        public void AgregarTarea(Tarea tarea)
+        {
+            Tareas.Add(tarea);
         }
     }
 }
