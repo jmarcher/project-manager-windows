54758f89d931bf8c9b65bbbd7e0bfdd4f6233b36
Joaquin Marcher <joaquin@marcher.com.uy>
2015-09-22 23:57:43 -0300

    Agregada la funcionalidad para marcar una etapa como finalizada

--- Gestion de proyectos/Dominio/Etapa.cs
+++ Gestion de proyectos/Dominio/Etapa.cs
@@ -7,11 +7,15 @@ namespace Dominio
 {
     public class Etapa
     {
+        public String Nombre { get; set; }
         public int Id { get; set; }
         public List<Tarea> Tareas { get; set; }
+        public bool Finalizada { get; private set; }
 
         public Etapa()
         {
+            Nombre = "[Nombre por defecto]";
+            Finalizada = false;
             Tareas = new List<Tarea>();
         }
 
@@ -35,5 +39,36 @@ namespace Dominio
         {
             Tareas.Add(tarea);
         }
+
+        public DateTime ObtenerFechaFinalizacion()
+        {
+            DateTime fecha = Tareas.First().FInicio;
+            fecha = fecha.AddDays(Tareas.First().DuracionPendiente);
+            foreach(Tarea t in Tareas)
+            {
+                DateTime fechaActual = t.FInicio;
+                fechaActual = fechaActual.AddDays(t.DuracionPendiente);
+
+                if (DateTime.Compare(fechaActual, fecha) > 0)
+                    fecha = fechaActual;
+            }
+           
+            return fecha;
+        }
+        public void MarcarFinalizada() {
+            if (TodasTareasFinalizadas())
+                Finalizada = true;
+        }
+        private bool TodasTareasFinalizadas()
+        {
+            bool retorno = true;
+            foreach (Tarea t in Tareas)
+            {
+                retorno = retorno && t.Finalizada;
+
+            }
+            return retorno;
+        }    
+    
     }
 }
