07d48aee6c7cfb0caece158d11d42975e7fc966c
joaquin <joaand4@gmail.com>
2015-09-25 02:11:42 -0300

    Se crea el patron Singleton con la lista de proyectos

---
+++ Gestion de proyectos/Dominio/Singleton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class Singleton
+    {
+        private static Singleton instanciaSingleton = null;
+        private List<Proyecto> listaProyectos;
+       
+        public static Singleton GetInstance()
+        {
+            if (instanciaSingleton == null)
+            {
+                instanciaSingleton = new Singleton();
+            }
+            return instanciaSingleton;
+        }
+    }
+}
