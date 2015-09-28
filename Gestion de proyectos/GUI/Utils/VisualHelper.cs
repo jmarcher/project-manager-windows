cd601b4b490ab464a68136b8e9f5fac91b5dd5a8
Joaquin Marcher <joaquin@marcher.com.uy>
2015-09-28 13:55:48 -0300

    Mejoradas las funcionlidades de la VentanaEtapa, ahora la misma guarda los cambios. ** Agregada la clase VisualHelper para tener ayudas visuales en la parte gráfica que serán usadas por todas las ventanas en conjunto, como los dialogos de confirmación.

---
+++ Gestion de proyectos/GUI/Utils/VisualHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUI.Utils
+{
+    public class VisualHelper
+    {
+        public static bool CartelConfirmacion(string Message, string Title)
+        {
+            DialogResult dialogResult = MessageBox.Show(Message,
+                Title, MessageBoxButtons.YesNo);
+            return dialogResult == DialogResult.Yes;
+        }
+    }
+}
