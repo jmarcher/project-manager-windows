4e2dc32548950e24b97362fb242ca05639350d76
Joaquin Marcher <joaquin@marcher.com.uy>
2015-10-03 20:20:43 -0300

    Prueba de una tarea simple

---
+++ Gestion de proyectos/UnitTests/TareaSimplePrueba.cs
@@ -0,0 +1,26 @@
+﻿using Xunit;
+using Dominio;
+using System;
+
+namespace PruebasUnitarias
+{
+    public class TareaSimplePrueba
+    {
+        [Theory]
+        [InlineData("Tarea","10/12/1990","10/15/1990")]
+        [InlineData("Tarea", "10/12/1990", "10/11/1990")]
+        [InlineData("Tarea", "10/12/1990", "10/12/1990")]
+        [InlineData("Tarea", "10/12/1990", "10/15/1990")]
+        public void CrearTareaSimpleFechasValidas(string nombre,
+            string fechaInicio,string fechaFin)
+        {
+            Tarea tarea = new TareaSimple()
+            {
+                Nombre = nombre,
+                FechaInicio = DateTime.Parse(fechaInicio),
+                FechaFin = DateTime.Parse(fechaFin)
+            };
+            Assert.NotNull(tarea);
+        }
+    }
+}
