8d0e7852a43709177abf7d607abeeb5fa94bd3de
Joaquin Marcher <joaquin@marcher.com.uy>
2015-10-31 18:34:34 -0300

    Prueba unitaria de persona

---
+++ Gestion de proyectos/PruebasUnitarias/PersonaPrueba.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace PruebasUnitarias
+{
+    public class PersonaPrueba
+    {
+        [Theory]
+        [InlineData("Jorge", "Administrador")]
+        [InlineData("Diego", "Un rol")]
+        [InlineData("Nombre","Rol")]
+        public void CrearPersona(string nombre, string rol)
+        {
+            Persona persona = new Persona()
+            {
+                Nombre = nombre,
+                Rol = rol
+            };
+
+            Assert.Equal(nombre, persona.Nombre);
+            Assert.Equal(rol, persona.Rol);
+        }
+    }
+}
