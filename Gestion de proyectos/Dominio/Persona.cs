d40bcdd240f14b9e3433dc840ceb1e98b6dbce13
Joaquin Marcher <joaquin@marcher.com.uy>
2015-10-31 18:39:35 -0300

    Código que pasa las pruebas de persona

---
+++ Gestion de proyectos/Dominio/Persona.cs
@@ -0,0 +1,10 @@
+﻿namespace Dominio
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+        public string Rol { get; set; }
+
+
+    }
+}
--- Gestion de proyectos/PruebasUnitarias/PersonaPrueba.cs
+++ Gestion de proyectos/PruebasUnitarias/PersonaPrueba.cs
@@ -1,4 +1,5 @@
 ﻿using Xunit;
+using Dominio;
 
 namespace PruebasUnitarias
 {
